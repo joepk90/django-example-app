8c441f2d28a15935a31bcabbf1596dd29e4a45a5
Joe <joeparkkennaby@gmail.com>
2022-01-23 23:39:18 +0000

    setup new endpoint: forms. setup drf_react_template FormSchemaViewSetMixin view set to return form data for posts

--- example_app/blog/views.py
+++ example_app/blog/views.py
@@ -1,6 +1,35 @@
+from rest_framework.mixins import RetrieveModelMixin, CreateModelMixin, UpdateModelMixin
 from rest_framework.viewsets import ModelViewSet
+from drf_react_template.mixins import FormSchemaViewSetMixin
 from . models import Post
 from . serializers import PostSerializer
+
+
+# Form View Set
+
+# list view: not allowed (don't want to return a list and the form schema)
+# http://host/blog/forms/
+
+# retrieve view: allowed
+# http://host/blog/forms/:id
+
+# TODO
+# work out how to request just the form data:
+# - use a query parameter (form=true) to includeFormSchemaViewSetMixin
+# - setup authentication? - stop the form data from being returned if not authenticated
+
+class PostFormViewSet(CreateModelMixin,
+                      #   DestroyModelMixin,
+                      #   ListModelMixin,
+                      RetrieveModelMixin,
+                      UpdateModelMixin,
+                      FormSchemaViewSetMixin):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    # class Meta:
+    #     model = Post
+    #     fields = ['id', 'title', 'content']
 
 
 class PostViewSet(ModelViewSet):
