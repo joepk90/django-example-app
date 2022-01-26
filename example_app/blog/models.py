33abe5e237c44182ebb44c839e77ef70af1c8faa
Joe <joeparkkennaby@gmail.com>
2022-01-26 22:29:33 +0000

    add last_update property DateTimeField to post model

--- example_app/blog/models.py
+++ example_app/blog/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=255)
+    last_update = models.DateTimeField(auto_now=True)
