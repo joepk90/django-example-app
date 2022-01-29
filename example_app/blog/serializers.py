afbb103320ea23021baa0e181873266d16aa18bd
Joe <joeparkkennaby@gmail.com>
2022-01-29 19:17:10 +0000

    update the PostSerializer to show the last_update field as a read only property

--- example_app/blog/serializers.py
+++ example_app/blog/serializers.py
@@ -3,6 +3,9 @@ from . models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+
+    last_update = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content']
+        fields = ['id', 'title', 'content', 'last_update']
